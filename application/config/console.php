0f50d4bc1a81c95797556cb3965446e72c8e4167
Yakovlev Vladimir <yakovlev.vladimir@hotmail.com>
2012-10-16 15:22:24 +1100

    Add readme & configs

--- application/config/console.php
+++ application/config/console.php
@@ -1,37 +1,33 @@
 <?php
 
-// This is the configuration for yiic console application.
-// Any writable CConsoleApplication properties can be configured here.
-return array(
-	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Console Application',
+return CMap::mergeArray(
+// наследуемся от main.php
+    require(dirname(__FILE__).'/main.php'),
+    array(
+        'components'=>array(
+            // переопределяем компонент db
+            'db'=>array(
 
-	// preloading 'log' component
-	'preload'=>array('log'),
+                'emulatePrepare' => true,
+                'username' => 'root',
+                'password' => '',
+            ),
+        ),
 
-	// application components
-	'components'=>array(
-		'db'=>array(
-			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-		),
-		// uncomment the following to use a MySQL database
-		/*
-		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
-			'emulatePrepare' => true,
-			'username' => 'root',
-			'password' => '',
-			'charset' => 'utf8',
-		),
-		*/
-		'log'=>array(
-			'class'=>'CLogRouter',
-			'routes'=>array(
-				array(
-					'class'=>'CFileLogRoute',
-					'levels'=>'error, warning',
-				),
-			),
-		),
-	),
+        'modules'=>array(
+            // uncomment the following to enable the Gii tool
+
+            'gii'=>array(
+                'class'=>'system.gii.GiiModule',
+                'password'=>'dev',
+                // If removed, Gii defaults to localhost only. Edit carefully to taste.
+                'ipFilters'=>array('127.0.0.1','::1'),
+
+                'generatorPaths'=>array(
+                    'bootstrap.gii',
+                ),
+            ),
+
+        ),
+    )
 );
