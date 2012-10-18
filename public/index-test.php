cda04b214b48ee8d5ae48d955c05074a3cb356a0
nakedman <egabrain@gmail.com>
2012-10-18 22:12:07 +1100

    Fixed paths

--- public/index-test.php
+++ public/index-test.php
@@ -5,8 +5,8 @@
  */
 
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/../kholmsk/library/yii/yii.php';
-$config=dirname(__FILE__).'/protected/config/test.php';
+$yii=dirname(__FILE__).'/../library/yii/yii.php';
+$config=dirname(__FILE__).'/../application/config/test.php';
 
 // remove the following line when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true);
