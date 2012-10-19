6a2f4617af202dd1fdb89406fd2837f32904a12d
Yakovlev Vladimir <yakovlev.vladimir@hotmail.com>
2012-10-19 17:22:11 +1100

    index-test.php open only localhost

--- public/index-test.php
+++ public/index-test.php
@@ -1,4 +1,7 @@
 <?php
+if ($_SERVER['REMOTE_ADDR'] != '127.0.0.1' || $_SERVER['REMOTE_ADDR'] != '::1/128')
+    die('Test open only localhost');
+
 /**
  * This is the bootstrap file for test application.
  * This file should be removed when the application is deployed for production.
