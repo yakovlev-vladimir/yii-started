7799116517ea969a56a61a164e0b8faeed55418d
Yakovlev Vladimir <yakovlev.vladimir@hotmail.com>
2012-12-02 16:19:11 +1100

    Create production config

---
+++ application/config/production.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Write: Yakovlev Vladimir | yakovlev.vladimir@hotmail.com
+ * Date: 02.12.12
+ * Time: 16:17
+ * TODO: Build full config
+ */
+return CMap::mergeArray(
+// наследуемся от main.php
+    require(dirname(__FILE__).'/main.php'),
+    array(
+        'components'=>array(
+            // переопределяем компонент db
+            'db'=>array(
+
+                'emulatePrepare' => true,
+                'username' => 'root',
+                'password' => '',
+            ),
+        ),
+
+        'modules'=>array(
+
+
+        ),
+    )
+);
